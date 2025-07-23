9b0721b458ddfed2b9b09b940feb4391f8c4cb3d
Sangini Verma <sanginiv11@gmail.com>
2025-07-24 01:20:57 +0530

    Update bitsetting.cpp

--- bitsetting.cpp
+++ bitsetting.cpp
@@ -12,3 +12,8 @@ int main() {
     cout << "After resetting bit " << bitPosition << ": " << (number & (~(1 << bitPosition))) << endl;
     return 0;
 }
+/*OUTPUT
+Enter an integer: 11
+Enter bit position to modify: 0
+After setting bit 0: 11
+After resetting bit 0: 10 */
